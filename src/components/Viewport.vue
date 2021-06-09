b775bd095a9d47a0de743ba07510de443df725d4
Hyrum Cooper <hyrumcoop@gmail.com>
2021-06-09 15:11:07 -0600

    setup composables in Viewport component

--- src/components/Viewport.vue
+++ src/components/Viewport.vue
@@ -1,67 +1,39 @@
 <template>
-  <div class='viewport'></div>
+  <div ref='viewport'></div>
 </template>
 
 <script>
-import * as THREE from 'three';
+import useRenderer from '@/composables/useRenderer';
+import useCameraControls from '@/composables/useCameraControls';
+import useLighting from '@/composables/useLighting';
+import useChain from '@/composables/useChain';
+import useMarbles from '@/composables/useMarbles';
 
 export default {
-  props: {
-    fov: {
-      type: Number,
-      default: 75
-    }
+  setup() {
+    const renderer = useRenderer();
+    const cameraControls = useCameraControls(renderer.camera, renderer.viewport, renderer.onAnimate);
+    const lighting = useLighting(renderer.scene);
+    const chain = useChain(renderer.scene);
+    const marbles = useMarbles(renderer.scene);
+    
+    return {
+      ...renderer,
+      ...cameraControls,
+      ...lighting,
+      ...chain,
+      ...marbles
+    };
   },
   mounted() {
-    this.init();
-  },
-  methods: {
-    init() {
-      new ResizeObserver(this.onResize).observe(this.$el);
-
-      const width = this.$el.offsetWidth;
-      const height = this.$el.offsetHeight;
-
-      this.scene = new THREE.Scene();
-      this.camera = new THREE.PerspectiveCamera(this.fov, width / height, 0.1, 1000);
-
-      this.renderer = new THREE.WebGLRenderer();
-      this.renderer.setSize(width, height);
-      this.$el.appendChild(this.renderer.domElement);
-
-      const geometry = new THREE.BoxGeometry();
-      const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-      this.cube = new THREE.Mesh(geometry, material);
-      this.scene.add(this.cube);
-
-      this.camera.position.z = 5;
-
-      this.animate()
-    },
-    animate() {
-      requestAnimationFrame(this.animate);
-
-      this.cube.rotation.x += 0.01;
-			this.cube.rotation.y += 0.01;
-
-      this.renderer.render(this.scene, this.camera);
-    },
-    onResize() {
-      const width = this.$el.offsetWidth;
-      const height = this.$el.offsetHeight;
-
-      this.camera.aspect = width / height;
-      this.camera.updateProjectionMatrix();
-
-      this.renderer.setSize(width, height);
-    }
+    this.animate();
   }
 }
 </script>
 
 <style scoped>
 
-.viewport {
+div {
   height: 100%;
 }
 
