e337636cac8df9bfd2683ae623f02b5d03a516a7
Hyrum Cooper <hyrumcoop@gmail.com>
2021-06-09 15:09:53 -0600

    created useChain composable

---
+++ src/composables/useChain.js
@@ -0,0 +1,30 @@
+import { onMounted } from 'vue';
+
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+const useChain = (scene) => {
+  const loader = new GLTFLoader();
+  const material = new THREE.MeshStandardMaterial({color: 0x00ff00});
+
+  const initChain = async () => {
+    const loadedData = await loader.loadAsync('./chain.glb');
+    const chainGeometry = loadedData.scene.children[0].geometry;
+
+    const chainLeft = new THREE.Mesh(chainGeometry, material);
+    const chainRight = new THREE.Mesh(chainGeometry, material);
+
+    chainRight.rotation.y = Math.PI;
+
+    scene.value.add(chainLeft);
+    scene.value.add(chainRight);
+  }
+
+  onMounted(initChain);
+
+  return {
+    initChain
+  }
+}
+
+export default useChain;
