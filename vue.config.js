4990056fcec9dde0d63c8cd81b137aefede1f414
Hyrum Cooper <hyrumcoop@gmail.com>
2021-07-10 12:37:54 -0600

    updated simulator title

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/chain-puzzle-simulator/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/chain-puzzle-simulator/' : '/',
+  chainWebpack: config => {
+    config
+        .plugin('html')
+        .tap(args => {
+            args[0].title = 'Chain Puzzle Simulator';
+            return args;
+        });
+  }
 }
